9aa3d15ebc07fcf24a56bfc2d4b76969f6a53c4d
Manohar <manoharchoppa6@gmail.com>
2025-03-29 23:22:23 +0530

    Version-2.6

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Wrap App here
+import { HashRouter } from 'react-router-dom'; // Change this
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter> {/* Wrap App inside Router here */}
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
