3dc97f8a63ced2018b0734d3b2e758afe88ec1d8
Manohar <manoharchoppa6@gmail.com>
2025-03-29 23:12:33 +0530

    Version-2.3

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Wrap App here
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter> {/* Wrap App inside Router here */}
+    <App />
+  </BrowserRouter>
 );
