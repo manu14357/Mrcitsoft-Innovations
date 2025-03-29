1a2a002929540371ae45c73546aae0595351d3fe
Manohar <manoharchoppa6@gmail.com>
2025-03-29 12:36:24 +0530

    Version-1.3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"/Mrcitsoft-Innovations/",
+  base:"/",
   optimizeDeps: {
     include: ['react-syntax-highlighter'],
   },
