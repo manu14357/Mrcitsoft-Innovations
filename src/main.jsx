6c81db988a22ba7ca5bc1f0d7ff67a92b28f43ae
Manohar <manoharchoppa6@gmail.com>
2025-03-29 23:19:43 +0530

    Version-2.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Change this
+import { BrowserRouter } from 'react-router-dom'; // Wrap App here
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter> {/* Use HashRouter instead of BrowserRouter */}
+  <BrowserRouter> {/* Wrap App inside Router here */}
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
