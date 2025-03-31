dcf49315ee0db5a97602762c9a4496b03437316f
Manohar <manoharchoppa6@gmail.com>
2025-03-31 09:01:59 +0530

    Version-2.8

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Change this
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
